d516fb095b9644bfef94b79e4e33337f498be0fd
Carlo <carlopennetta91@gmail.com>
2021-03-09 17:22:42 +0100

    build login backend

---
+++ db/config.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "d3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "otxnyvygp21lkomw";
+$password = "gezr6w9b05fx3x8i";
+$dbname = "rx2te87mm7vnu0ny";
+
+?>
