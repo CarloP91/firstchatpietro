2f6b10f66aef40dc0650e9e61e708d9f20138b6a
Pietro <pietro.1993@icloud.com>
2021-03-11 15:38:39 +0100

    change php

--- index.php
+++ index.php
@@ -33,33 +33,7 @@ include("db/config.php");
     <span id="orologio"></span>
     <span id="data"></span>
 
-    <div class="container">
-        <img src="./img/profile.png" alt="Avatar">
-        <p>Hello. How are you today?</p>
-        <span class="time-right">11:00</span>
-      </div>
-      
-      <div class="container darker">
-        <img src="./img/hacker.png" alt="Avatar">
-        <p>Hey! I'm fine. Thanks for asking!</p>
-        <span class="time-left">11:01</span>
-      </div>
-      
-      <div class="container">
-        <img src="./img/profile.png" alt="Avatar">
-        <p>Sweet! So, what do you wanna do today?</p>
-        <span class="time-right">11:02</span>
-      </div>
-      
-      <div class="container darker">
-        <img src="./img/hacker.png" alt="Avatar">
-        <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
-        <span class="time-left">11:05</span>
-      </div>
-
-      <input type="text" name="written" id="written">
-      <input type="submit" name="send" id="send">
- 
+   
 
    
    
