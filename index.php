f95fc33d4884bfa00fbc811123a417bd88c3c6fa
Carlo <carlopennetta91@gmail.com>
2021-03-11 16:14:09 +0100

    fix login and home

--- index.php
+++ index.php
@@ -29,24 +29,11 @@ include("db/config.php");
         <?php include("login.php"); ?>
     </form>
  
-
     <span id="orologio"></span>
     <span id="data"></span>
-
-   
-
-   
-   
-   
+ 
 </body>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-<script>
 
-
-   
-
-
-    
-</script>
 </html>
