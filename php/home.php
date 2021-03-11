411348dc4166ef3ea8893092b9acc4597ef98637
Pietro <pietro.1993@icloud.com>
2021-03-11 12:38:08 +0100

    Qui ho realizzato il css della pagina

---
+++ php/home.php
@@ -0,0 +1,185 @@
+
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+          <head>
+            <meta charset="utf-8">
+            <title></title>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" href="style.css">
+          </head>
+          <body>
+            <header>
+            <div class="container">
+              <div class="row no-gutters bg-white my-4">
+                <div class="col-lg-4 d-flex flex-column flex-wrap">
+        
+                  <div class="myProfile d-flex justify-content-between align-items-center text-secondary px-2">
+                    <img src="https://www.repstatic.it/content/nazionale/img/2017/07/23/235932206-7c6cce30-252c-4aac-b369-7cf331aef410.jpg" class="rounded float-left rounded-circle ml-1">
+                    <div class="symbols d-flex justify-content-end">
+                      <i class="far fa-circle"></i>
+                      <i class="fas fa-comment-alt"></i>
+                      <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                  </div>
+        
+                  <div class="chat d-flex flex-column flex-wrap">
+                    <div class="searchChat d-flex justify-content-center align-items-center text-secondary">
+                      <input type="search" placeholder="Cerca o inizia una nuova chat" class="search">
+                    </div>
+        
+                    <div class="container_Chat">
+        
+                      <div class="privateChat" data-chat-name="0">
+                        <img src="https://scontent.cdninstagram.com/vp/118dd38f543246a78d58da1cb62cad6c/5D4DAFF5/t51.2885-15/e35/s480x480/53123411_308771629782383_1211351969503362355_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com" class="rounded float-left rounded-circle">
+                        <div class="singleChat float-left pl-3">
+                          <div class="name text-bold">
+                            Damian Lillard
+                          </div>
+                          <div class="message text-secondary">
+                            <span class="messaggio_corrente">Yeah, Man!</span>
+                          </div>
+                        </div>
+                        <small class="float-right">19:02</small>
+                      </div>
+        
+                      <div class="privateChat" data-chat-name="1">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkBETSMVzMeSCIPa9P7WzDreaVgZM1BVEejVqZXDLDEr6fnPLU" class="rounded float-left rounded-circle">
+                        <div class="singleChat float-left pl-3">
+                          <div class="name text-bold">
+                            Lebonwskii
+                          </div>
+                          <div class="message text-secondary">
+                            <i class="fas fa-check"></i> <span class="messaggio_corrente">You: Bomba!</span>
+                          </div>
+                        </div>
+                        <small class="float-right">18:59</small>
+                      </div>
+        
+                      <div class="privateChat" data-chat-name="2">
+                        <img src="https://www.caffeinamagazine.it/wp-content/uploads/2016/04/0-giuratoHome.jpg" class="rounded float-left rounded-circle">
+                        <div class="singleChat float-left pl-3">
+                          <div class="name text-bold">
+                            <span class="messaggio_corrente">Luca Giurato
+                          </div>
+                          <div class="message text-secondary">
+                            <i class="fas fa-check"></i>  <span class="messaggio_corrente"> You: a pra foco</span>
+                          </div>
+                        </div>
+                        <small class="float-right">19:16</small>
+                      </div>
+        
+                      <div class="privateChat" data-chat-name="3">
+                        <img src="https://pbs.twimg.com/profile_images/3217340962/3fcb99000a3c32796342b3541a44de9e_400x400.png" class="rounded float-left rounded-circle">
+                        <div class="singleChat float-left pl-3">
+                          <div class="name text-bold">
+                            Swarley
+                          </div>
+                          <div class="message text-secondary">
+                            <i class="fas fa-check"></i> <span class="messaggio_corrente">You: Ndo sei?</span>
+                          </div>
+                        </div>
+                        <small class="float-right">18:48</small>
+                      </div>
+        
+                      <div class="privateChat" data-chat-name="4">
+                        <img src="https://specials-images.forbesimg.com/imageserve/5b1547204bbe6f74868b7e64/416x416.jpg?background=000000&cropX1=1260&cropX2=3735&cropY1=504&cropY2=2979" class="rounded float-left rounded-circle">
+                        <div class="singleChat float-left pl-3">
+                          <div class="name text-bold">
+                            KD
+                          </div>
+                          <div class="message text-secondary">
+                            <span class="messaggio_corrente"> GSW REIGN!</span>
+                          </div>
+                        </div>
+                        <small class="float-right">11:48</small>
+                      </div>
+        
+                      <div class="privateChat" data-chat-name="5">
+                        <img src="https://pbs.twimg.com/profile_images/1118058186639126528/_YPpV9t1_400x400.jpg" class="rounded float-left rounded-circle">
+                        <div class="singleChat float-left pl-3">
+                          <div class="name text-bold">
+                            Mihawk
+                          </div>
+                          <div class="message text-secondary">
+                            <span class="messaggio_corrente">ti piacerebbe</span>
+                          </div>
+                        </div>
+                        <small class="float-right">11:48</small>
+                      </div>
+        
+                      <div class="privateChat" data-chat-name="6">
+                        <img src="https://ae01.alicdn.com/kf/HTB19CT0NVXXXXX7aXXXq6xXFXXXH/Anime-One-Piece-Donquixote-Doflamingo-Joker-Sunglasses-Cosplay-Glasses-Eyewear-Props-Accessories-3-colors-Fashion-Limted.jpg" class="rounded float-left rounded-circle">
+                        <div class="singleChat float-left pl-3">
+                          <div class="name text-bold">
+                            Do Flamingo
+                          </div>
+                          <div class="message text-secondary">
+                            <span class="messaggio_corrente">ti aspetto a dressRosa</span>
+                          </div>
+                        </div>
+                        <small class="float-right">09:40</small>
+                      </div>
+        
+                    </div>
+                  </div>
+                </div>
+        
+                <div class="col-lg-8 d-flex flex-column flex-wrap">
+        
+                  <div class="openChat">
+                    <div class="titleChat d-flex align-items-center p-3">
+                      <img src="https://scontent.cdninstagram.com/vp/118dd38f543246a78d58da1cb62cad6c/5D4DAFF5/t51.2885-15/e35/s480x480/53123411_308771629782383_1211351969503362355_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com" class="rounded float-left rounded-circle">
+                      <div class="singleChat float-left pl-3">
+                        <div class="namec text-bold">Damian Lillard</div>
+                        <small class="text-secondary">online</small>
+                      </div>
+                      <div class="chatOptions ml-auto float-right">
+                        <i class="fas fa-search"></i>
+                        <i class="fas fa-paperclip"></i>
+                        <i class="fas fa-ellipsis-v"></i>
+                      </div>
+                    </div>
+        
+                    <div class="containChat d-flex justify-content-center align-items-center py-3">
+                      <div class="conversation d-flex">
+        
+                      </div>
+                    </div>
+        
+                    <div class="userKeyboard d-flex flex-row justify-content-around align-items-center text-secondary">
+                      <i class="far fa-smile"></i>
+                      <input type="text" class="rounded send" placeholder="Scrivi un messaggio">
+                      <i class="fas fa-microphone"></i>
+                    </div>
+        
+                  </div>
+                </div>
+              </div>
+            </div>
+        
+            <div class="template-message">
+              <div class="new-message">
+                <span class="text-message"></span>
+                <div class="info-message">
+                  <span class="time-message"></span>
+                  <i class="fas fa-check"></i>
+                </div>
+                <div class="message-options">
+                  <div class="cancella">
+                    Eliminare il messaggio?
+                  </div>
+                  <div class="opzioni d-flex justify-content-between text-success">
+                    <span class="goback">Annulla</span>
+                    <span class="delete">Elimina</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </header>
+        
+        
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+            <script src="script.js" charset="utf-8"></script>
+          </body>
+        </html>
