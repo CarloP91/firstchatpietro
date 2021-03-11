c1641588204cd396c2b569d530bd89569813eb79
Carlo <carlopennetta91@gmail.com>
2021-03-11 17:22:28 +0100

    insert and select bkd and front css

--- home.php
+++ home.php
@@ -11,6 +11,7 @@ session_start();
      <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
+
     <title>HOME PAGE CHAT PIETRO</title>
 </head>
 <body>
@@ -23,6 +24,34 @@ session_start();
     <input type="textarea" id="messaggio">
     <button id="send_msg">INVIA</button>
 
+    <?php include "db/select_chat_msg.php" ?>
+
+
+
     
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("#send_msg").click(function(){
+
+    let mitt = $("#mittente").val();
+    let mess = $("#messaggio").val();
+    if ($("#messaggio").val() != "") {
+      $.ajax({
+          type: 'POST',
+          url: "db/insert_db_chat_mitt.php",
+          data: {"mittente": mitt, "messaggio" : mess},
+          success: function(data){
+            console.log("Dati inviati");
+          },
+
+          error: function(data) {
+            console.log("Dati non inviati");
+          }
+      });
+    }
+  });
+});
+</script>
 </html>
